5e90bbe6e5b2929cf5fca28f218fe907af8fbf02
Sharang Ukidve <ukidve.sharang@gmail.com>
2020-09-26 20:21:53 +0530

    Fix -- IconButton: moving click event on button

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,12 +1,10 @@
 <template>
-  <button class="icon-button">
-    <i
-      class="material-icons"
-      @click.stop="emitEvent('click.stop', $event)"
-      @click="emitEvent('click', $event)"
-      :style="{ fontSize: size }"
-      ><slot></slot
-    ></i>
+  <button
+    class="icon-button"
+    @click.stop="emitEvent('click.stop', $event)"
+    @click="emitEvent('click', $event)"
+  >
+    <i class="material-icons" :style="{ fontSize: size }"> <slot></slot></i>
   </button>
 </template>
 
